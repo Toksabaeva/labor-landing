2b0881292909e9b2b49bf5799b649fa129641ff7
unknown <tleuzhan.toksabayeva@ffin.kz>
2023-01-03 23:55:31 +0600

    addin element ui and layouts

---
+++ src/layout/components/Sidebar/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <logo :collapse="isCollapse" />
+    <el-scrollbar wrap-class="scrollbar-wrapper">
+      <el-menu
+        :default-active="activeMenu"
+        :collapse="isCollapse"
+        :background-color="variables.menuBg"
+        :text-color="variables.menuText"
+        :unique-opened="false"
+        :active-text-color="variables.menuActiveText"
+        :collapse-transition="false"
+        mode="vertical"
+      >
+        <sidebar-item v-for="route in permission_routes" :key="route.path" :item="route" :base-path="route.path" />
+      </el-menu>
+    </el-scrollbar>
+  </div>
+</template>
