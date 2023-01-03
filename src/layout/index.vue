db1494492a75441aecae56d6e4984fd239c38e36
unknown <tleuzhan.toksabayeva@ffin.kz>
2023-01-03 23:55:07 +0600

    addin element ui and layouts

---
+++ src/layout/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app-wrapper">
+    <div class="drawer-bg" @click="handleClickOutside" />
+    <sidebar class="sidebar-container" />
+    <div class="main-container">
+      <app-main />
+    </div>
+  </div>
+</template>
+
+<script>
+import { AppMain, Sidebar } from "./components";
+
+export default {
+  name: "LayoutBlock",
+  components: {
+    AppMain,
+  },
+  methods: {
+    handleClickOutside() {
+      this.$store.dispatch("app/closeSideBar", { withoutAnimation: false });
+    },
+  },
+};
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-
+import Layout from "@/layout";
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -24,6 +24,19 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import("../views/LoginView.vue"),
+    },
+    {
+      path: "/",
+      component: Layout,
+      redirect: "/profile",
+      children: [
+        {
+          path: "profile",
+          component: () => import("@/views/AboutView.vue"),
+          name: "about",
+          meta: { title: "profile", icon: "profile", affix: true },
+        },
+      ],
     },
   ],
 });
